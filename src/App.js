8d8f66728b4c2b141a901e43605c3d2cf1937e67
rcubillo <ronaldcubillo92@gmail.com>
2019-09-18 08:43:52 -0500

    Added the react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, {Component} from 'react';
 import {BrowserRouter as Router, Route, Linlk} from "react-router-dom" 
 import "bootstrap/dist/css/bootstrap.min.css";
+import TodosList from "./components/todos-list.component";
+import EditTodo from "./components/edit-todo.component";
+import CreateTodo from "./components/create-todo.component";
 
 class App extends Component {
   render() {
@@ -8,10 +11,10 @@ class App extends Component {
     <Router>
    <div className="container">
     <h2>Todo List</h2>
-    </div>
-      <Route path="/" exact component={TodosList} />
-      <Route path="/edit/:id" component={EditTodo} />
+    <Route path="/" exact component={TodosList} />
+    <Route path="/edit/:id" component={EditTodo} />
       <Route path="/create" component={CreateTodo} />
+    </div>
     </Router>
   );
   }
